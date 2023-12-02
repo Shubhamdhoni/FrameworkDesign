187977f313c59199c95e834c528084f62ede89ce
praveen <praveenhudgi1@gmail.com>
2023-12-02 10:28:29 +0530

    Updated to main branch

--- src/test/java/com/org/loginfeature/TestCase1_Login.java
+++ src/test/java/com/org/loginfeature/TestCase1_Login.java
@@ -22,6 +22,8 @@ public class TestCase1_Login extends BaseTestComponents {
 		loginPm.sendKeys(loginPm.userName, userName);
 		loginPm.sendKeys(loginPm.password, password);
 		loginPm.clickLoginButton();
+		
+		System.out.println("Added 2nd changes");
 
 		
 		
